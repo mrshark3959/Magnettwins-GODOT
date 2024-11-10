c4dcefc8cb22fcb872b233415a759dc0e367bd5d
Dungcannon <graftedrevenant@gmail.com>
2024-11-10 18:25:13 -0500

    made whole thing good :thumbs_up:

--- scripts/Movement.gd
+++ scripts/Movement.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-@onready var _animated_sprite = $AnimatedSprite2D
 
-
-
-const SPEED = 120.0
-const JUMP_VELOCITY = -220.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 const PUSH_FORCE := .0
 const MIN_PUSH_FORCE := 5.0
+
+func _ready() -> void:
+	get_node("Texture").play("default")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,28 +15,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up_m") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left_m", "right_m")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.x > 0:
-			scale = Vector2(1,1)
+			scale = Vector2(3,3)
 			rotation_degrees = 0
 		if velocity.x < 0:
-			scale = Vector2(1,-1)
+			scale = Vector2(3,-3)
 			rotation_degrees = 180
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-				
-	if direction != 0 && _animated_sprite.animation != "run_red":
-		_animated_sprite.play("run_red")
-	if direction == 0 && _animated_sprite.animation != "idle_red":
-		_animated_sprite.play("idle_red")
-		
+	
+	if direction != 0 && get_node("Texture").animation != "walk":
+		get_node("Texture").play("walk")
+	if direction == 0 && get_node("Texture").animation != "default":
+		get_node("Texture").play("default")
 
 	move_and_slide()
 	
