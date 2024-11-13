0762ca9a9df52cc290d62b0dc88fba2fe4079879
Midnite59 <awsomekai789@gmail.com>
2024-11-13 18:21:43 -0500

    11/13/24

--- trigger.gd
+++ trigger.gd
@@ -1,5 +1,5 @@
 extends Area2D
-var snapbody
+var snapbody = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,8 +19,13 @@ func _process(delta: float) -> void:
 			get_node("Pulse").animation = "default"
 			#print("bye")
 	if snapbody is RigidBody2D && Input.is_action_pressed("space"):
-		snapbody.position = global_position
+		
+		snapbody.global_transform.origin = global_position
+		snapbody.linear_velocity = Vector2(0,0)
+		set_gravity_space_override_mode(Area2D.SPACE_OVERRIDE_DISABLED)
 	else: 
+		if snapbody is RigidBody2D:
+			snapbody.freeze = false
 		snapbody = null
 
 
@@ -30,5 +35,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_snap_body_entered(body: Node2D) -> void:
-	if Input.is_action_pressed("space"):
+	if Input.is_action_pressed("space") && snapbody == null && gravity_space_override == Area2D.SPACE_OVERRIDE_REPLACE:
 		snapbody = body
