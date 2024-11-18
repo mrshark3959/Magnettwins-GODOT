6a930b0074c2e83ac558098966faca878ff2a06d
dungcannon <graftedrevenant@gmail.com>
2024-11-17 23:47:14 -0500

    Added new physics from Mechbranch-Snap.
     Odd problem w/ the gravblock retaining momentum, not sure if this is intentional or not... If not, fix next patch.

--- scripts/trigger.gd
+++ scripts/trigger.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+var snapbody
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,8 +18,16 @@ func _process(delta: float) -> void:
 		if get_node("Pulse").animation == "pulse":
 			get_node("Pulse").animation = "default"
 			#print("bye")
-
+	if snapbody is RigidBody2D && Input.is_action_pressed("space"):
+		snapbody.position = global_position
+	else: 
+		snapbody = null
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
 		body.apply_central_impulse(Vector2(0.1, 0.1))
+		
+func _on_snap_body_entered(body: Node2D) -> void:
+	if Input.is_action_pressed("space"):
+		snapbody = body
+	
