e822daccb88d4ba8ef5c1ba7500d150846792b2e
mrshark3959 <alfonsotheshark@gmail.com>
2024-12-25 18:53:37 -0500

    pulleyplaceholder

---
+++ Scripts/pulley.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var animation = $pulleymovement
+@onready var pulleyblockright = $pulleyblockright
+@onready var pulleyblockleft = $pulleyblockleft
+
+var animation_played = false  # Flag to track if animation has played
+
+func _ready() -> void:
+	# Set initial positions
+	pulleyblockright.position = Vector2(66, -58)
+	pulleyblockleft.position = Vector2(0, 0)
+	
+	# Connect animation_finished signal
+	animation.animation_finished.connect(_on_animation_finished)
+
+func _on_area_2d_body_entered(body: RigidBody2D) -> void:
+	# Play animation only if it hasn't played before
+	if not animation_played:
+		animation.play("pulleymovement")
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	# Pause the animation only if it hasn't finished
+	if not animation_played:
+		animation.pause()
+
+func _on_animation_finished(anim_name: String) -> void:
+	# Lock positions and disable physics movement
+	animation_played = true  # Mark animation as played
+	
+	# Set both blocks to Static mode to lock them in place
+	pulleyblockright.position = Vector2(66,0)
+	pulleyblockleft.position = Vector2(0,-58)
