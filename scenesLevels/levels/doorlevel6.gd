0b11d9c44c8f58ecee69dd7d68eaeb45f8e53ae8
mrshark3959 <alfonsotheshark@gmail.com>
2025-01-11 08:35:38 -0500

    a

---
+++ scenesLevels/levels/doorlevel6.gd
@@ -0,0 +1,18 @@
+extends AnimatableBody2D
+
+@onready var maincollision = $CollisionShapeclosed
+@onready var animation = $AnimatedSprite2D
+@onready var characterdetector = $characterdetector
+
+# Variable to track whether the animation has been played
+var animationplayed = false
+
+func _ready() -> void:
+	animation.play("idle")
+	maincollision.set_deferred_thread_group("enabled",1) 
+func _on_characterdetector_body_entered(body: CharacterBody2D) -> void:
+	if not animationplayed:
+		animation.play("open")
+		animationplayed = true
+		
+		maincollision.set_deferred_thread_group("disabled",1) 
