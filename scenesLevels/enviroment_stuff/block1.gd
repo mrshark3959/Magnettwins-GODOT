e7e5cd89bd3666d8f63fad63f6a928f00b4e539a
mrshark3959 <alfonsotheshark@gmail.com>
2025-01-17 22:05:22 -0500

    a

--- scenesLevels/enviroment_stuff/block1.gd
+++ scenesLevels/enviroment_stuff/block1.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@onready var block = $"."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	custom_integrator = true  # Enable the custom integrator
@@ -7,7 +8,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	apply_central_impulse(Vector2(0.1, 0.1))  # Apply a central force every frame
-
+	#block.linear_velocity.y = 0
 # Implement custom physics behavior
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	state.angular_velocity = 0  # Prevent rotation
