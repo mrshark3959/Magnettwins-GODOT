c030218bf2f5fd81c413c98c7a77f9f8acf6978b
dungcannon <graftedrevenant@gmail.com>
2025-01-19 13:09:49 -0500

    fixed ferrus

---
+++ scripts/fellout.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+
+
+
+
+func respawn():
+	var retree = get_tree()
+	retree.call_deferred("reload_current_scene")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print(area.name)
+	if "Snap" in area.name or "Trigger" in area.name:
+		print("ferrus trigger detected")
+	elif "ferrusarea" in area.name:
+		respawn()	
+	else:
+		print("Something has fallen")
+		respawn()
